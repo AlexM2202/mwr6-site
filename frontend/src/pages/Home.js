bc88daa7f4dc75380902e44f8629ee867ee79af9
Matthias Mackay <mattmack108@gmail.com>
2025-07-15 00:41:57 -0400

    added twitch stream on home page if its live, and changed the media page to be more efficient (only loads once)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,43 @@
+import { useEffect } from "react";
+
 function Home(){
+    useEffect(() => {
+        const embedIfLive = async () => {
+            /*checks if the embed does not already exists, and then checks if live
+            and if so, then embeds stream */
+            let existingTwitchEmbed = document.getElementById("twitch-embed");
+            if (!existingTwitchEmbed) {
+                const homeDiv = document.querySelector(".Home");
+                const div = document.createElement("div");
+                div.id = "twitch-embed";
+                homeDiv.appendChild(div);
+
+                let response = await fetch("https://decapi.me/twitch/uptime/midwestr6");
+                if (!((await response.text()).includes("midwestr6"))) {
+                    console.log("LIVE");
+                    const script = document.createElement('script');
+                    script.src = "https://embed.twitch.tv/embed/v1.js";
+                    script.async = true;
+
+                    script.onload = () => {
+                        new window.Twitch.Embed("twitch-embed", {
+                            width: "60%",
+                            height: 625,
+                            channel: "midwestr6",
+                            layout: "video",
+                        });
+                    };
+                    document.body.appendChild(script);
+                } else {
+                    console.log("Not live");
+                }
+            }
+        };
+        embedIfLive();
+    }, []);
+
     return (
-        <div className="Home">
-            <h2>
-                Welcome to the Home Page
-            </h2>
-        </div>
+        <div className="Home"></div>
     );
 }
 
--- frontend/src/pages/Media.js
+++ frontend/src/pages/Media.js
@@ -1,19 +1,22 @@
+import { useEffect } from "react";
 import './Media.css'
 
 function Media(){
-    const script = document.createElement('script');
-    script.src = "https://embed.twitch.tv/embed/v1.js";
-    script.async = true;
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://embed.twitch.tv/embed/v1.js";
+        script.async = true;
 
-    window.onload = () => {
-        new window.Twitch.Embed("twitch-embed", {
-            width: "60%",
-            height: 625,
-            channel: "midwestr6",
-            layout: "video",
-        });
-    }
-    document.body.appendChild(script);
+        window.onload = () => {
+            new window.Twitch.Embed("twitch-embed", {
+                width: "60%",
+                height: 625,
+                channel: "midwestr6",
+                layout: "video",
+            });
+        }
+        document.body.appendChild(script);
+    }, []);
     
     return (
         <div className="Media">
